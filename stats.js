e499f1e53dd1b918bf93878128e9c1622b0d52e4
harryyy27 <harryyy27@gmail.com>
2019-03-10 11:45:31 +0000

    median

--- stats.js
+++ stats.js
@@ -1,20 +1,66 @@
+//median
+const median = (arr)=> {
+    if(!Array.isArray(arr)){
+        throw new TypeError('Please use an array')
+    }
+    else if(!arr.every((el)=>isNaN(el)===false)){
+        throw new TypeError('All elements of array should be numbers')
+    }
+    const sorted = arr.sort((a,b)=> a-b)
+    if(sorted.length%2===0){
+        return (sorted[sorted.length/2+1]+sorted[sorted.length/2-1])/2
+    }
+    else {
+        return sorted[sorted.length/2-0.5]
+    }
+}
+//mean
 const mean = (arr) => {
     if(!Array.isArray(arr)){
         throw new TypeError('Please use an array')
     }
-    else if(!arr.every((el)=>typeof el === 'number')){
+    else if(!arr.every((el)=>isNaN(el)===false)){
         throw new TypeError('All elements of array should be numbers')
     }
     return arr.reduce((acc,cur)=> acc+cur)/arr.length
-    
 }
-
-// const std = (arr) => {
-//     try {
-
-//     }
-// }
+//population variance
+const popVar = (arr) => {
+    try {
+        return arr.reduce((acc,cur)=>acc+Math.pow(cur,2))/arr.length-Math.pow(mean(arr),2);
+    }
+    catch(e){
+        return "Please ensure array containing only numbers provided"
+    }
+}
+//sample variance 
+const samVar = (arr) =>{
+    try {
+        return arr.reduce((acc,cur)=>acc+Math.pow(cur,2))/(arr.length-1)-Math.pow(mean(arr),2);
+    }
+    catch(e){
+        return "Please ensure array containing only numbers provided"
+    }
+}
+//population standard deviation
+const popStd = (arr) => {
+    try {
+        Math.pow(popVar(arr),0.5)
+    }
+    catch(e){
+        return "Please ensure array containing only numbers provided"
+    }
+}
+//sample standard deviation
+const samStd = (arr) => {
+    try {
+        Math.pow(samVar(arr),0.5)
+    }
+    catch(e){
+        return "Please ensure array containing only numbers provided"
+    }
+}
 
 console.log(mean([1,1,2,2]))
 
-module.exports = {mean}
+module.exports = {median,mean,popVar,samVar,popStd,samStd}
--- test.js
+++ test.js
@@ -1,18 +1,42 @@
-const {mean} = require('./stats')
+const {median,mean,popVar,samVar,popStd,samStd} = require('./stats')
 
 
 test('adds 1 + 2 to equal 3', () => {
   expect(2+1).toBe(3);
 });
+//median 
+test('Enterring anything but an array to median function will throw an error', ()=>{
+    const throwError = () => {
+        median(undefined)
+    }
+    expect(throwError).toThrow(new TypeError('Please use an array'))
+})
+test('If array contains non-numeric elements, error thrown', ()=>{
+    
+    const throwError = ()=>{
+        median([NaN, 2, 5, 10])
+    }
+    expect(throwError).toThrow(new TypeError('All elements of array should be numbers'))
+})
+test('Median of odd numbered array is central number', ()=>{
+    expect(median([1,2,3,4,5])).toBe(3);
+})
+test('Median of even numbered array is equal to the average of the 2 central numbers',()=>{
+    expect(median([1,1,1,3,3,3])).toBe(2);
+})
+test('Median of unordered array = median of sorted array',()=>{
+    expect(median([3,2,1,4,5])).toBe(3);
+})
+
 
 test('Enterring anything but an array will throw an error', ()=>{
-    let throwError = ()=>{
+    const throwError = ()=>{
         mean(2)
     }
     expect(throwError).toThrowError(new TypeError('Please use an array'))
 });
 test('If array contains non-numeric elements, error thrown', ()=>{
-    let throwError = ()=>{
+    const throwError = ()=>{
         mean(['a',1,3,4])
     }
     expect(throwError).toThrowError(new TypeError('All elements of array should be numbers'))
